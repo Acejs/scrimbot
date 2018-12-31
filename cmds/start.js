49cb639c90226720682fbdb4d69af0ec1f5b33b5
Acejs <46245956+Acejs@users.noreply.github.com>
2018-12-30 22:49:14 -0500

    Update start.js

--- cmds/start.js
+++ cmds/start.js
@@ -38,11 +38,7 @@ module.exports.run = async (bot, message, args) => {
     let editTime = "";
 
     let timeEmbed = new Discord.RichEmbed()
-        .setTitle("SNIPE MATCH STARTING
-Alert:
-- A snipe match is starting, pay attention!
-Instructions:
-- We will do a countdown from 3 sec and you will ready up on go.")
+        .setTitle("SNIPE MATCH STARTING Alert: - A snipe match is starting, pay attention! Instructions: - We will do a countdown from 5 sec and you will ready up on go.")
         .setColor("#FF3333");
 
     setTimeout(async () => {
